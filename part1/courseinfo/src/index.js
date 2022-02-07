13157631b4c859154c1e3e6dea778d494defebc0
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-02-07 12:54:32 +0800

    exercise 2.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = ({ course }) => {
-  return <h1>{course.name}</h1>
+  return <h2>{course.name}</h2>
 }
 
 const Total = ({ course }) => {
@@ -44,34 +44,59 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((item) => (
+        <Course course={item} key={item.id} />
+      ))}
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
