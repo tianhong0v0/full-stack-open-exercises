e2aa33e5f1b2fabf0e086569d83840d49769bd59
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-02-07 11:20:02 +0800

    exercise 2.1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -26,9 +26,9 @@ const Content = ({ course }) => {
   console.log('from Content: ', parts)
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      {parts.map((part) => (
+        <Part part={part} key={part.id} />
+      ))}
     </div>
   )
 }
