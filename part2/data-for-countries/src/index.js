b011d9714609b1eee3e9bc3c49afe996a97b28eb
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-02-14 09:02:18 +0800

    use numOfRes to decide whether display one or many

--- part2/data-for-countries/src/index.js
+++ part2/data-for-countries/src/index.js
@@ -2,10 +2,15 @@ import ReactDOM from 'react-dom'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Display from './components/Display'
+import DisplaySingleCountry from './components/DisplayOneCountry'
+import DisplayCountries from './components/DisplayCountries'
 
 const App = () => {
   const [inputQuery, setInputQuery] = useState('')
   const [allResponseCountries, setAllResponseCountries] = useState([])
+  //const [displaySingle, setDisplaySingle] = useState(false)
+  //const [tooMany, setTooMany] = useState(false)
+  const [numberOfResponse, setNumberOfResponse] = useState(0)
   //execute useEffect after changing inputQuery
   useEffect(() => {
     console.log('input query: ', inputQuery)
@@ -13,12 +18,17 @@ const App = () => {
     axios
       .get(query)
       .then((response) => response.data)
+      .then((data) => setAllResponseCountries(data))
       .catch((err) => {
         //TO-DO: Distinguish when inputQuery is empty or cannot find certain country
         console.error(err.response.status)
         setAllResponseCountries([])
       })
   }, [inputQuery])
+
+  useEffect(() => {
+    setNumberOfResponse(allResponseCountries.length)
+  }, [allResponseCountries])
 
   const changeHandler = (event) => {
     setInputQuery(event.target.value)
@@ -30,9 +40,42 @@ const App = () => {
         find countries
         <input onChange={changeHandler} />
       </label>
-      <Display countries={allResponseCountries} />
+      {/* <Display countries={allResponseCountries} /> */}
+      {numberOfResponse > 10 && (
+        <div>Too many matches, specity another filter</div>
+      )}
+      {numberOfResponse === 1 && (
+        <DisplaySingleCountry country={allResponseCountries[0]} />
+      )}
+      {numberOfResponse <= 10 && numberOfResponse > 1 && (
+        <DisplayCountries countries={allResponseCountries} />
+      )}
     </div>
   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+/*import ReactDOM from 'react-dom'
+import React, { useState } from 'react'
+
+const AddTripButton = ({ addTrip }) => {
+  return <button onClick={addTrip}> Add a trip</button>
+}
+
+const AnotherComponent = () => <div>added!!!</div>
+
+const App = () => {
+  const [state, setState] = useState('start')
+
+  return (
+    <div>
+      {state === 'start' && (
+        <AddTripButton addTrip={() => setState('add-trip')} />
+      )}
+      {state === 'add-trip' && <AnotherComponent />}
+    </div>
+  )
+} 
+
+ReactDOM.render(<App />, document.getElementById('root')) */
