cfffe98f3c9c78b652fa95ecad82b07f679557f3
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-02-18 15:42:35 +0800

    exercise 2.16: extract phonebook services

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,41 +3,19 @@ import React, { useEffect, useState } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
-import axios from 'axios'
+//import axios from 'axios'
+import phonebookService from './services/phonebook'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then((response) => setPersons(response.data))
-  }, [])
-  /* Notice that
-  we create two independent state variables: newName, newNumber 
-
-  Why not create a single [newPerson, setNewPerson] state? 
-  (in which that newPerson object includes two fields: newName and newNumber)
-
-  If you use this.setState in a class, 
-  which merges the updated fields into object automatically
-  in other words, this.setState updates partial object.  
-  It makes more sense to create a single newPerson object
-  
-  However, if you are using useState(), 
-  there is no automatic merging, 
-  you will have to merge these fields (name, number, ...) 
-  into the previous state object manually. 
-
-  Separating independent state variables also has another benefit.
-  It makes it easy to later extract some related logic into a custom Hook */
-
-  //But, May be I should not seperate filter and applyFilter?
-  //Since they always change together
   const [filter, setFilter] = useState('')
   const [applyFilter, setApplyFilter] = useState(false)
+
+  useEffect(() => {
+    phonebookService.getAll().then((response) => setPersons(response))
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -51,7 +29,6 @@ const App = () => {
   const handleFilterChange = (event) => {
     let inputChars = event.target.value
     if (inputChars.length > 0) {
-      //is there another way to check if the input is empty?
       setApplyFilter(true)
       setFilter(inputChars.toLowerCase())
     } else {
@@ -61,7 +38,7 @@ const App = () => {
   }
 
   const addPerson = (event) => {
-    event.preventDefault() //Pay attention to this!!!
+    event.preventDefault()
     if (persons.some((item) => item.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
@@ -69,10 +46,9 @@ const App = () => {
         name: newName, //using name as key
         number: newNumber,
       }
-      axios
-        .post(`http://localhost:3001/persons`, personObject)
-        .then((response) => setPersons(persons.concat(response.data)))
-      //setPersons(persons.concat(personObject))
+      phonebookService
+        .create(personObject)
+        .then((response) => setPersons(persons.concat(response)))
     }
     setNewName('')
     setNewNumber('')
