b5c2fd5882dd8b18d11d1d0ff8e7949d5ebcab4f
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-06 13:40:02 +0800

    utils/logger&middleware module

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,18 +1,26 @@
 const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+
 console.log('connecting to', config.MONGODB_URI)
 mongoose
   .connect(config.MONGODB_URI)
-  .then((result) => console.log('Successfully connected to database'))
-  .catch((error) => console.log(error))
+  .then((result) => logger.info('Successfully connected to database'))
+  .catch((error) => logger.error(error))
 
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
