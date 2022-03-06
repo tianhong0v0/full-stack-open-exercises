b8a9b6d4488f83b4d900166e510199c290116582
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-06 14:06:18 +0800

    exercise 4.4: test totalLikes

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
   return 1
 }
+const totalLikes = (array) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return array.length === 0 ? 0 : array.reduce(reducer, 0)
+}
 
 module.exports = {
   dummy,
+  totalLikes,
 }
