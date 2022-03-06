6710f89b77b042e8da7016f0a8fcbf3eac273574
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-06 13:31:21 +0800

    controller/blogs module

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,31 +3,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/Blog')
-
+const blogsRouter = require('./controllers/blogs')
 console.log('connecting to', config.MONGODB_URI)
 mongoose
   .connect(config.MONGODB_URI)
-  .then((result) => console.log(`Successfully connected to database`))
+  .then((result) => console.log('Successfully connected to database'))
   .catch((error) => console.log(error))
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-    .catch((error) => console.log(error))
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
