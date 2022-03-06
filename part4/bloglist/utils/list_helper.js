7f0c589ca7b48bc0064c6b97a85f8886deeb4849
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-03-06 14:32:31 +0800

    exercise 4.5: test favoriteBlog

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,6 +1,8 @@
 const dummy = (blogs) => {
+  console.log(blogs)
   return 1
 }
+
 const totalLikes = (array) => {
   const reducer = (sum, item) => {
     return sum + item.likes
@@ -9,7 +11,18 @@ const totalLikes = (array) => {
   return array.length === 0 ? 0 : array.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const fav = blogs.reduce(
+    (tempFav, item) => (tempFav.likes > item.likes ? tempFav : item),
+    blogs[0]
+  )
+  const result = { title: fav.title, author: fav.author, likes: fav.likes }
+  console.log(JSON.stringify(result))
+  return result
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
