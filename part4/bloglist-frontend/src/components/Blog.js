209d61525560080ccf3be4dbbc818a7684275ea7
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2022-04-26 13:58:40 +0800

    ex 5.7: show or hide details of a blog

--- part4/bloglist-frontend/src/components/Blog.js
+++ part4/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  const [showDetails, setShowDetails] = useState(false)
+
+  const summary = () => (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={() => setShowDetails(!showDetails)}>view</button>
+      </div>
+    </div>
+  )
+
+  const details = () => (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <br></br>
+        {blog.url}
+        <br></br>
+        {blog.likes}
+        <br></br>
+        {blog.author}
+        <button onClick={() => setShowDetails(!showDetails)}>hide</button>
+      </div>
+    </div>
+  )
+
+  return <div>{showDetails ? details() : summary()}</div>
+}
 
 export default Blog
